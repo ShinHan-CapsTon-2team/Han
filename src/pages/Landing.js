8de4daf3abefc07b5b7772c61835c454538c5bd2
youjung <kelly000128@naver.com>
2023-08-07 15:08:35 +0900

    landing_0807

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,7 +2,7 @@ import logo from '../Images/imagelogo.png'
 import photoreco from '../Images/image 20.png'
 import fitphoto from '../Images/image 19.png'
 import gogohome from '../Images/image 18.png'
-
+import gopostup from '../Images/postup.png'
 import { useNavigate } from 'react-router-dom';
 
 import styled from "styled-components";
@@ -10,18 +10,12 @@ import styled from "styled-components";
 
 function Re_Landing(){
 
+    
     const navigate = useNavigate();
     //퀴즈 
     const handleFitPhotoClick = () => {
         navigate('/quizindex');
     };
-
-
-    const screenWidth = window.screen.width;
-    const screenHeight = window.screen.height;
-
-    console.log(`현재 웹 페이지 해상도 너비: ${screenWidth}px`);
-    console.log(`현재 웹 페이지 해상도 높이: ${screenHeight}px`); 
 
     
     //홈페이지
@@ -32,7 +26,11 @@ function Re_Landing(){
     const handleUploadPhotoClick = () => {
         navigate('/reco');
     };
-    
+    // 사진 등록
+    const handleUpPhotoClick = () => {
+        navigate('/post');
+    };
+    //const OutWrap = styled.div``;
 
     const OutWrap = styled.div`
     width: 100%;
@@ -43,7 +41,6 @@ function Re_Landing(){
     flex-direction: column;
     justify-content: center;
     align-items: center;
-
     
 `;
 
@@ -57,40 +54,33 @@ function Re_Landing(){
     `;
 
     const LogoImg = styled.img`
-        @media (min-width: 1920px) and (max-width: 1080px) {} {
-         width: 90%;
-         height: 100%;
-        }
-
         width: 55%;
-        height: 274px; /* height 값에 px 단위를 추가하세요 */
-        margin-top: 100px; /* marginTop -> margin-top */
-        margin-bottom: 50px; /* marginBottom -> margin-bottom */
+        height: 60%; /* height 값에 px 단위를 추가하세요 */
+        margin-top: 50px; /* marginTop -> margin-top */
+        margin-bottom: 20px; /* marginBottom -> margin-bottom */
 
-       
+        @media (min-width: 1920px) and (min-width: 1080px)  {
+            width: 70%;
+            height: 60%;
+        }
     `;
 
     const ImgWrap = styled.div`
         text-align: center;
         display: flex;
         flex-direction: column;
-        
         align-items: center;
-
-        
-        
     `;
 
     const Img = styled.img`
-        width: 45%;
+        width: 55%;
         height: auto;
         margin-bottom: 16px; /* marginBottom -> margin-bottom */
 
-        @media (min-width: 1920px) and (max-width: 1080px) {} {
-            width: 100%;
-            height: 100%;
-           }
-       
+        @media (min-width: 1920px) and (min-width: 1080px) {
+            width: 70%;
+            
+        }
     `;
 
 
@@ -104,8 +94,8 @@ function Re_Landing(){
                 <ImgWrap>
                     <Img src={photoreco} onClick ={handleUploadPhotoClick} alt='' />
                     <Img src={fitphoto} alt=''onClick={handleFitPhotoClick}/>
-                    <Img src={gogohome} onClick={handleGohomeClick} alt=''/>
-                    
+                    <Img src={gogohome} onClick={handleGohomeClick} alt=''/> 
+                    <Img src={gopostup} onClick={handleUpPhotoClick} alt=''/> {/* 사진 등록 추가 */}
                 </ImgWrap>
                 
             </InsideWrap>
